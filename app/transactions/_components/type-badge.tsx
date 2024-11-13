8cb288cf7813a4007ad235f4680b7c63718ebbaa
Erick De Paula <erickricardowork@gmail.com>
2024-11-13 00:58:39 -0300

    feat: ensure transaction table labels are rendered properly

--- app/transactions/_components/type-badge.tsx
+++ app/transactions/_components/type-badge.tsx
@@ -7,24 +7,24 @@ interface TransactionTypeBadgeProps {
 }
 
 const TransactionTypeBadge = ({ transaction }: TransactionTypeBadgeProps) => {
-  if ((transaction.type = TransactionType.DEPOSIT)) {
+  if (transaction.type === TransactionType.DEPOSIT) {
     return (
-      <Badge className="bg-muted font-bold text-primary hover:bg-muted">
+      <Badge className="bg-muted font-bold text-primary hover:bg-muted/20">
         <CircleIcon className="mr-2 fill-primary" size={10} />
         Depósito
       </Badge>
     );
   }
-  if ((transaction.type = TransactionType.EXPENSE)) {
+  if (transaction.type === TransactionType.EXPENSE) {
     return (
-      <Badge className="bg-danger bg-opacity-10 font-bold text-danger">
+      <Badge className="bg-danger bg-opacity-10 font-bold text-danger hover:bg-danger/20">
         <CircleIcon className="mr-2 fill-danger" size={10} />
         Despesa
       </Badge>
     );
   }
   return (
-    <Badge className="bg-white bg-opacity-10 font-bold text-white">
+    <Badge className="bg-white bg-opacity-10 font-bold text-white hover:bg-white/20">
       <CircleIcon className="mr-2 fill-white" size={10} />
       Investimento
     </Badge>
