218ec71ec04c76a9683c971d1adf03746a048af3
Erick De Paula <erickricardowork@gmail.com>
2024-11-17 08:04:45 -0300

    feat: add transaction pie chart componet

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,6 +4,8 @@ import Navbar from "../_components/navbar";
 import SummaryCards from "./_componets/summary-cards";
 import TimeSelect from "./_componets/time-select";
 import { isMatch } from "date-fns";
+import TransactionsPieChart from "./_componets/transactions-pie-chart";
+import { getDashboard } from "../_data/get-dashboard";
 
 interface HomeProps {
   searchParams: {
@@ -20,7 +22,7 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
   if (monthIsInvalid) {
     redirect("?month=01");
   }
-
+  const dashboard = await getDashboard(month);
   return (
     <>
       <Navbar />
@@ -29,7 +31,14 @@ const Home = async ({ searchParams: { month } }: HomeProps) => {
           <h1 className="text-2xl font-bold">Dashboard</h1>
           <TimeSelect />
         </div>
-        <SummaryCards month={month} />
+        <div className="grid grid-cols-[2fr,1fr]">
+          <div className="flex flex-col gap-6">
+            <SummaryCards month={month} {...dashboard} />
+            <div className="grid grid-cols-3 grid-rows-1 gap-6">
+              <TransactionsPieChart {...dashboard} />
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
