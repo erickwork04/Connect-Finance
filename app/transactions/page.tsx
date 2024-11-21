db48382b33bd99eeffc5c9af7a556ac3e106d807
Erick De Paula <erickricardowork@gmail.com>
2024-11-21 18:01:28 -0300

    fix: ensure user can scroll on transactions page

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -27,13 +27,13 @@ const TransitionsPage = async () => {
   return (
     <>
       <Navbar />
-      <div className="space-y-6 overflow-hidden p-6">
+      <div className="flex flex-col space-y-6 overflow-hidden p-6">
         {/*Título e botão*/}
         <div className="flex w-full items-center justify-between">
           <h1 className="text-2xl font-bold">Transações</h1>
           <AddTransactionButton userCanAddTransaction={userCanAddTransaction} />
         </div>
-        <ScrollArea>
+        <ScrollArea className="h-full">
           <DataTable
             columns={transactionColumns}
             data={JSON.parse(JSON.stringify(transactions))}
